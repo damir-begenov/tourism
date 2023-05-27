7b4a4619718c0e4c1af8cc4c7281fbbdb03b6fa4
Damir <damir.ps04@gmail.com>
2023-05-28 02:21:05 +0600

    'fe'

--- src/main/java/com/example/tourism/Controller.java
+++ src/main/java/com/example/tourism/Controller.java
@@ -5,9 +5,57 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping("/")
-    public String getAstana(){
+    @GetMapping("/astana")
+    public String getAstana() {
         return "astana";
     }
 
+    @GetMapping("/grandmap")
+    public String grandmap() {
+        return "grandmap";
+    }
+
+    @GetMapping("/almaty")
+    public String almaty() {
+        return "almaty";
+    }
+
+    @GetMapping("/bayanaul")
+    public String bayanaul() {
+        return "bayanaul";
+    }
+
+    @GetMapping("/borovoe")
+    public String borovoe() {
+        return "borovoe";
+    }
+
+    @GetMapping("/imantau-shalkar")
+    public String imantaushalkar() {
+        return "imantau-shalkar";
+    }
+
+    @GetMapping("/east-kazakhstan")
+    public String eastkazakhstan() {
+        return "east-kazakhstan";
+    }
+
+    @GetMapping("/balkash")
+    public String balkash() {
+        return "balkash";
+    }
+
+    @GetMapping("/turkestan")
+    public String turkestan() {
+        return "turkestan";
+    }
+
+    @GetMapping("/ulitau")
+    public String ulitau() {
+        return "ulitau";
+    }
+    @GetMapping("/mangistau")
+    public String mangistau() {
+        return "mangistau";
+    }
 }
